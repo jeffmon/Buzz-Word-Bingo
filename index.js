75937ea4d6ab260a8c3e1bd5a0c35f28fad19f39
jeffmon <jeffmon@gmail.com>
2017-07-13 19:44:16 -1000

    added dot notation and missing semicolons

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ const bodyParser = require("body-parser");
 var collection = [];
 var score = 0;
 
-app.use(express.static("./public/"))
+app.use(express.static("./public/"));
 app.use(bodyParser.urlencoded());
 
 function getWords() {
   var wordsOnly = [];
   for (var i = 0; i < collection.length; i++) {
-    wordsOnly.push(collection[i]["buzzWord"]);
+    wordsOnly.push(collection[i].buzzWord);
   }
   return wordsOnly;
 }
 
 function receiveWord(obj) {
-  var number = Number(obj["points"]);
-  obj["points"] = number;
-  obj["heard"] = false;
+  var number = Number(obj.points);
+  obj.points = number;
+  obj.heard = false;
   collection.push(obj);
   console.log(collection);
 }
 
 function putWord(obj) {
   for (var i = 0; i < collection.length; i++) {
-    if (collection[i]["buzzWord"].toLowerCase() === obj["buzzWord"].toLowerCase()) {
-      collection[i]["heard"] = true;
-      score += collection[i]["points"];
+    if (collection[i].buzzWord.toLowerCase() === obj.buzzWord.toLowerCase()) {
+      collection[i].heard = true;
+      score += collection[i].points;
     }
   }
 }
 
 function putAgain(obj) {
   for (var i = 0; i < collection.length; i++) {
-    if (collection[i]["buzzWord"].toLowerCase() === obj["buzzWord"].toLowerCase()) {
-      if (collection[i]["heard"] === true) {
+    if (collection[i].buzzWord.toLowerCase() === obj.buzzWord.toLowerCase()) {
+      if (collection[i].heard === true) {
         return false;
       } else {
         return true;
@@ -48,27 +48,27 @@ function putAgain(obj) {
 
 function deleteWord(obj) {
   var location = collection.findIndex(function(o) {
-    return o["buzzWord"] === obj["buzzWord"];
-  })
+    return o.buzzWord === obj.buzzWord;
+  });
   collection.splice(location, 1);
   console.log(collection);
 }
 
 app.get("/", (req, res) => {
   res.send("./index.html");
-})
+});
 
 app.get("/buzzwords", (req, res) => {
   res.json({
     "buzzWords": getWords()
   });
-})
+});
 
 app.route("/buzzword")
   .post((req, res) => {
     var isWordThere = collection.some(function(obj) {
-      return obj["buzzWord"].toLowerCase() === req.body["buzzWord"].toLowerCase()
-    })
+      return obj.buzzWord.toLowerCase() === req.body.buzzWord.toLowerCase();
+    });
     if (isWordThere || collection.length === 5) {
       res.json({
         "success": false
@@ -82,38 +82,38 @@ app.route("/buzzword")
   })
   .put((req, res) => {
     var isWordThere = collection.some(function(obj) {
-      return obj["buzzWord"].toLowerCase() === req.body["buzzWord"].toLowerCase()
+      return obj.buzzWord.toLowerCase() === req.body.buzzWord.toLowerCase();
     });
     if (isWordThere && putAgain(req.body)) {
       putWord(req.body);
       res.json({
         "success": true,
         "newScore": score
-      })
+      });
     } else{
       res.json({
         "success": false
-      })
+      });
     }
 
   })
   .delete((req, res) => {
     var isWordThere = collection.some(function(obj) {
-      return obj["buzzWord"].toLowerCase() === req.body["buzzWord"].toLowerCase()
+      return obj.buzzWord.toLowerCase() === req.body.buzzWord.toLowerCase();
     });
     if (isWordThere) {
       deleteWord(req.body);
       res.json({
         "success": true
-      })
+      });
     } else {
       res.json({
         "success": false
-      })
+      });
     }
-  })
+  });
 
 const server = app.listen(8080, function() {
   var host = server.address().address;
   var port = server.address().port;
-})
+});
